f61e2889a55e4ddc7d98d35b94e0d947eba25bd1
mauro <mauro@mauro-PC>
2013-09-04 09:59:25 -0300

    pantalla grafica de bienvenida

---
+++ src/objetos/Empresa.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author mauro
+ * OBJETO QUE CONTIENE LOS PARAMETROS DE CONFIGURACION DE LA EMPRESA QUE EJECUTA EL SISTEMA
+ * 
+ */
+public class Empresa {
+    
+}

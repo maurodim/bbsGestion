279699ccce55f64ffc7b711427f48936f9df8261
mauro <mauro@mauro-PC>
2013-09-04 11:24:22 -0300

    pantalla grafica de bienvenida

---
+++ src/interfaces/Transaccionable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mauro
+ */
+public interface Transaccionable {
+    public Boolean guardarRegistro(String sql);//puede ser utiliazdo para insert o update
+    public Boolean guardarRegistros(ArrayList listadoDeSentencias);//puede ser utiliazdo para insert o update
+    public ArrayList leerConjuntoDeRegistros(String sql);
+    
+    
+    
+}

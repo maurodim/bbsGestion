409fd40316722c8def82c5033f6af711ba5c6108
mauro <mauro@mauro-PC>
2013-09-04 11:38:19 -0300

    pantalla grafica de bienvenida

--- src/objetos/Conecciones.java
+++ src/objetos/Conecciones.java
@@ -5,7 +5,14 @@
 package objetos;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import interfaces.Transaccionable;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,8 +22,9 @@ import java.sql.Connection;
  * PARA QUE AQUI SE REALICE LA TRANSACCION
  * 
  */
-public class Conecciones {
+public class Conecciones implements Transaccionable{
     private Connection con;
+    private Statement st;
 
     public Conecciones() {
                 MysqlDataSource dataSource=new MysqlDataSource();
@@ -27,6 +35,7 @@ public class Conecciones {
                     dataSource.setPassword("");
                     dataSource.setServerName("localhost");
                     this.con=dataSource.getConnection();
+                    Statement st=this.con.createStatement();
                  }catch(Exception ex){
                     
                 String cod1=String.valueOf(ex);
@@ -35,6 +44,43 @@ public class Conecciones {
 		}
 
         
+    }
+
+    @Override
+    public Boolean guardarRegistro(String sql) {
+        Boolean coneccion=true;
+        try {
+            
+            this.st.executeQuery(sql);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Conecciones.class.getName()).log(Level.SEVERE, null, ex);
+            coneccion=false;
+        }
+        return coneccion;
+    }
+
+    @Override
+    public Boolean guardarRegistros(ArrayList listadoDeSentencias) {
+        String sql="";
+        Boolean coneccionCorrecta=true;
+        Iterator il=listadoDeSentencias.listIterator();
+        while(il.hasNext()){
+            sql=(String)il.next();
+            try {
+                this.st.executeUpdate(sql);
+            } catch (SQLException ex) {
+                Logger.getLogger(Conecciones.class.getName()).log(Level.SEVERE, null, ex);
+                coneccionCorrecta=false;
+            }
+        }
+        
+        return coneccionCorrecta;
+    }
+
+    @Override
+    public ArrayList leerConjuntoDeRegistros(String sql) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     }
